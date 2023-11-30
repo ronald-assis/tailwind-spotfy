14bc6eeca9e8c719aece8faad62f6ea96ba0b888
Ronald Assis <ronaldassis71@gmail.com>
2023-11-30 12:58:21 -0300

    feat: starting spotfy clone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main className="">
-      <h1>hello world!</h1>
-    </main>
+    <div className="flex h-screen flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-950 p-6">aside</aside>
+        <main className="flex-1 p-6">main</main>
+      </div>
+      <footer className="border-t border-zinc-700 bg-zinc-800 p-6">
+        footer
+      </footer>
+    </div>
   )
 }
